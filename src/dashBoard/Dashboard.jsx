e3b445dd4941a363c83af1e68e25bc98c2c76077
Shubham Shrivastwa <kumarraunak4201@gmail.com>
2024-11-24 09:34:05 -0800

    sidebar

--- src/dashBoard/Dashboard.jsx
+++ src/dashBoard/Dashboard.jsx
@@ -7,24 +7,38 @@ const Dashboard = () => {
 
   return (
     <div className="dashboard_con">
-      <Sidebar />
+      <div className="side_large  d-none d-lg-flex d-xl-flex d-md-flex">
+        <Sidebar />
+      </div>
+      {/* <Sidebar /> */}
 
       <div className="Dbody w-100 m-1">
         <nav className="w-100 d-flex px-2 d-xl-none d-lg-none d-md-none justify-content-between align-items-center">
-          <img src="/programming.png" className="box_shadow p-1 custome_border m-1" alt="" />
-          <Menu type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling" />
+          <img
+            src="/programming.png"
+            className="box_shadow p-1 custome_border m-1"
+            alt=""
+          />
+          <Menu
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasScrolling"
+            aria-controls="offcanvasScrolling"
+          />
         </nav>
       </div>
 
-
       {/* canvas */}
-      <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
+      <div
+        class="offcanvas offcanvas-start p-0"
+        data-bs-scroll="true"
+        data-bs-backdrop="false"
+        tabindex="-1"
+        id="offcanvasScrolling"
+        aria-labelledby="offcanvasScrollingLabel"
+      >
         <div class="offcanvas-body">
-          <p>Try scrolling the rest of the page to see this option in action.</p>
+            <Sidebar />
         </div>
       </div>
     </div>
--- src/dashBoard/sidebar/Sidebar.jsx
+++ src/dashBoard/sidebar/Sidebar.jsx
@@ -1,133 +1,209 @@
-import React, { useState } from 'react';
-import {  Award, Check, ChevronDown, DollarSign, FileText, Mail, Menu, MessageCircle, Sun, User, X } from "react-feather";
-import './Sidebar.scss';
+import React, { useState } from "react";
+import {
+  Award,
+  Check,
+  ChevronDown,
+  DollarSign,
+  FileText,
+  Mail,
+  Menu,
+  MessageCircle,
+  PhoneCall,
+  Sun,
+  User,
+  X,
+} from "react-feather";
+import "./Sidebar.scss";
+import { Phone } from "lucide-react";
 
 const Sidebar = () => {
-    const [toggle, setToggle] = useState(true);
-  
-    const hideSideBar = () => {
-      setToggle(!toggle);
-    };
+  const [toggle, setToggle] = useState(true);
+
+  const hideSideBar = () => {
+    setToggle(!toggle);
+  };
   return (
-    <div className={`Dsidebar m-1 d-none d-lg-flex d-xl-flex d-md-flex custome_radius box_shadow ${toggle ? "show" : "hide"}`}>
-        <div className="header d-flex align-items-center px-2">
-          <img
-            width={"40px"}
-            height={"40px"}
-            className={`rounded-circle ${toggle ? "d-block" : "d-none"}`}
-            src="https://cdn-icons-png.flaticon.com/256/8904/8904881.png"
-            alt=""
-          />
-          <Menu onClick={hideSideBar} />
-        </div>
-
-        {/* for the sidebar body */}
-        <div className="sideBody">
-        <ul>
-           <li className="li_profile">
-              <img className={`box_shadow profile custome_border p-1 m-2 ${toggle ? 'd-block' : 'd-none'}`} src="/programming.png" alt="" />
-           </li>
-           <li className={`${toggle ? '' : 'hide_text'}`}>
-              <User />
-               <span className={`${toggle ? 'd-block' : 'd-none'}`}>Hello Bunny</span>
-           </li>
-
-           <li  className={`${toggle ? '' : 'hide_text'}`}>
-               <Mail />
-               <span className={`${toggle ? 'd-block' : 'd-none'}`}> Connect@Shubham@gmail.com </span>
-           </li>
-
-           <li  className={`${toggle ? '' : 'hide_text'}`}>
-               <Award />
-               <span className={`${toggle ? 'd-block' : 'd-none'}`}> Fullstack Developer </span>
-           </li>
-
-           <li className={`${toggle ? '' : 'hide_text'}`}>
-               <FileText />
-               <select name="" id="" className={`w-100 h-100 border-0 ${toggle ? 'd-block' : 'd-none'} `}>
-               <option value="Past Projects">Past Projects</option>
-                 <option value="E-Commerce">E-Commerce</option>
-                 <option value="E-Commerce">E-Commerce</option>
-                 <option value="E-Commerce">E-Commerce</option>
-               </select>
-           </li>
-
-           <li className={`${toggle ? '' : 'hide_text'}`}>
-               <FileText />
-               <select name="" id="" className={`w-100 h-100 border-0 ${toggle ? 'd-block' : 'd-none'} `}>
-               <option value="Recent Projects">Recent Projects</option>
-                 <option value="E-Commerce">E-Commerce</option>
-                 <option value="E-Commerce">E-Commerce</option>
-                 <option value="E-Commerce">E-Commerce</option>
-               </select>
-           </li>
-
-           <li className={`${toggle ? '' : 'hide_text'}`} style={{color:'var(--green)'}}>
-               <MessageCircle />
-               <span className= {`${toggle ? 'd-block' : 'd-none'}`}>Chat Now</span>
-           </li>
-
-           <li className={`${toggle ? '' : 'hide_text'}`}>
-                 <details className="w-100">
-                   <summary className={`w-100 d-flex justify-content-center  `}>
-                       <DollarSign className={`${toggle ? 'me-2' : ''}`}/>
-                       <div className={`d-flex w-100 justify-content-between align-items-center ${toggle ? 'd-block' : 'd-none'} `}>
-                          <span>Payment Status</span>
-                          <ChevronDown className="i20" />
-                       </div>
-                   </summary>
-                   <div className="d-flex flex-column rounded shadow summary-bg mt-1">
-                      <ul className={`${toggle ? 'd-flex' : 'd-none'}`}>
-                        <li className="d-flex justify-content-between px-2 ">
-                          <span>Initial Payment</span>
-                          <Check className="ryt" />
-                        </li>
-
-                        <li className="d-flex justify-content-between px-2">
-                          <span>Half Payment</span>
-                          <X className="cross" />
-                        </li>
-
-                        <li className="d-flex justify-content-between px-2">
-                          <span>Full Payment</span>
-                          <X className="cross" />
-                        </li>
-                        
-                      </ul>
-                   </div>
-                 </details>
-           </li>
-
-           <li className={`${toggle ? '' : 'hide_text'}`}>
-                 <details className="w-100">
-                   <summary className={`w-100 d-flex justify-content-center  `}>
-                       <DollarSign className={`${toggle ? 'me-2' : ''}`}/>
-                       <div className={`d-flex w-100 justify-content-between align-items-center ${toggle ? 'd-block' : 'd-none'} `}>
-                          <span>Payment Status</span>
-                          <ChevronDown className="i20" />
-                       </div>
-                   </summary>
-                   <div className="d-flex flex-column rounded shadow summary-bg mt-1">
-                      <ul className={`${toggle ? 'd-flex' : 'd-none'}`}>
-                        <li className="d-flex justify-content-between px-2 ">
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, quaerat.
-                        </li>
-
-                      </ul>
-                   </div>
-                 </details>
-           </li>
-
-        </ul>
-        </div>
-
-        {/* for the footer */}
-        <div className="footer d-flex  align-items-center px-2">
-          <span className={`${toggle ? "d-block" : "d-none"}`}>Back</span>
-          <Sun />
-        </div>
+    <div
+      className={`Dsidebar m-1 d-flex custome_radius box_shadow ${
+        toggle ? "show" : "hide"
+      }`}
+    >
+      <div className="header d-flex align-items-center px-2">
+        <img
+          width={"40px"}
+          height={"40px"}
+          className={`rounded-circle ${
+            toggle ? "d-xl-block d-lg-block d-md-block d-none" : "d-none"
+          }`}
+          src="https://cdn-icons-png.flaticon.com/256/8904/8904881.png"
+          alt=""
+        />
+        <Menu
+          className="d-xl-block d-lg-block d-md-block d-none"
+          onClick={hideSideBar}
+        />
       </div>
-  )
-}
 
-export default Sidebar
+      {/* for the sidebar body */}
+      <div className="sideBody">
+        <ul>
+          <li className="li_profile">
+            <img
+              className={`box_shadow profile custome_border p-1 m-2 ${
+                toggle ? "d-block" : "d-none"
+              }`}
+              src="/programming.png"
+              alt=""
+            />
+          </li>
+          <li className={`${toggle ? "" : "hide_text"}`}>
+            <div className="d-flex align-items-center h-100 px-3 gap-2">
+              <User />
+              <span className={`${toggle ? "d-block" : "d-none"}`}>
+                Hello Bunny
+              </span>
+            </div>
+          </li>
+
+          <li className={`${toggle ? "" : "hide_text"}`}>
+            <div className="d-flex align-items-center h-100 px-3 gap-2">
+              <Mail />
+              <span className={`${toggle ? "d-block" : "d-none"}`}>
+                {" "}
+                Connect@Shubham@gmail.com{" "}
+              </span>
+            </div>
+          </li>
+
+          <li className={`${toggle ? "" : "hide_text"}`}>
+            <div className="d-flex align-items-center h-100 px-3 gap-2">
+              <Award />
+              <span className={`${toggle ? "d-block" : "d-none"}`}>
+                {" "}
+                Fullstack Developer{" "}
+              </span>
+            </div>
+          </li>
+
+          <li className={`${toggle ? "" : "hide_text"}`}>
+            <div className="d-flex align-items-center w-100 h-100 ps-3 pe-3 gap-2">
+              <FileText />
+              <select
+                name=""
+                id=""
+                className={`w-100 h-100 border-0 ${
+                  toggle ? "d-block" : "d-none"
+                } `}
+              >
+                <option value="Past Projects">Past Projects</option>
+                <option value="E-Commerce">E-Commerce</option>
+                <option value="E-Commerce">E-Commerce</option>
+                <option value="E-Commerce">E-Commerce</option>
+              </select>
+            </div>
+          </li>
+
+          <li className={`${toggle ? "" : "hide_text"}`}>
+            <div className="d-flex align-items-center w-100 h-100 ps-3 pe-3 gap-2">
+              <FileText />
+              <select
+                name=""
+                id=""
+                className={`w-100 h-100 border-0 ${
+                  toggle ? "d-block" : "d-none"
+                } `}
+              >
+                <option value="Recent Projects">Recent Projects</option>
+                <option value="E-Commerce">E-Commerce</option>
+                <option value="E-Commerce">E-Commerce</option>
+                <option value="E-Commerce">E-Commerce</option>
+              </select>
+            </div>
+          </li>
+
+          <li
+            className={`${toggle ? "" : "hide_text"}`}
+            style={{ color: "var(--green)" }}
+          >
+            <div className="d-flex align-items-center h-100 px-3 gap-2">
+              <MessageCircle />
+              <span className={`${toggle ? "d-block" : "d-none"}`}>
+                Chat Now
+              </span>
+            </div>
+          </li>
+
+          <li className={`${toggle ? "" : "hide_text"}`}>
+            <div className="d-flex align-items-center w-100 h-100 ps-3 pe-3 gap-2">
+              <details className="w-100">
+                <summary className={`w-100 d-flex justify-content-center  `}>
+                  <DollarSign className={`${toggle ? "me-2" : ""}`} />
+                  <div
+                    className={`d-flex w-100 justify-content-between align-items-center ${
+                      toggle ? "d-block" : "d-none"
+                    } `}
+                  >
+                    <span>Payment Status</span>
+                    <ChevronDown className="i20" />
+                  </div>
+                </summary>
+                <div className="d-flex flex-column rounded shadow summary-bg mt-1">
+                  <ul className={`${toggle ? "d-flex" : "d-none"}`}>
+                    <li className="d-flex justify-content-between px-2 ">
+                      <span>Initial Payment</span>
+                      <Check className="ryt" />
+                    </li>
+
+                    <li className="d-flex justify-content-between px-2">
+                      <span>Half Payment</span>
+                      <X className="cross" />
+                    </li>
+
+                    <li className="d-flex justify-content-between px-2">
+                      <span>Full Payment</span>
+                      <X className="cross" />
+                    </li>
+                  </ul>
+                </div>
+              </details>
+            </div>
+          </li>
+
+          <li className={`${toggle ? "" : "hide_text"}`}>
+            <div className="d-flex align-items-center w-100 h-100 ps-3 pe-3 gap-2">
+              <details className="w-100">
+                <summary className={`w-100 d-flex justify-content-center  `}>
+                  <PhoneCall className={`${toggle ? "me-2" : ""}`} />
+                  <div
+                    className={`d-flex w-100 justify-content-between align-items-center ${
+                      toggle ? "d-block" : "d-none"
+                    } `}
+                  >
+                    <span>Contact</span>
+                    <ChevronDown className="i20" />
+                  </div>
+                </summary>
+                <div className="d-flex flex-column rounded shadow summary-bg mt-1">
+                  <ul className={`${toggle ? "d-flex" : "d-none"}`}>
+                    <li className="d-flex justify-content-between px-2 ">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Officiis, quaerat.
+                    </li>
+                  </ul>
+                </div>
+              </details>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      {/* for the footer */}
+      <div className="footer d-flex  align-items-center px-2">
+        <span className={`${toggle ? "d-block" : "d-none"}`}>Back</span>
+        <Sun />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
